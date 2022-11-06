f8bf7f57c4f5deee0fd8086a21ce5eda8ee29a04
Noel Illing <illing.noel@gmail.com>
2022-11-06 09:25:49 -0800

    lets start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react"
 
 function App() {
+  const [hello, setHello] = useState()
+
+  function getPhoto() {
+    fetch("https://google.com", {dfgdf})
+    .then((response) => {
+      console.log(response)
+    })
+
+  }
+
   return (
     <div className="App">
       
