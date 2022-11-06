a2cf0416500fe915699fa4e661db1fbcadefe903
Henry <henrykim966@gmail.com>
2022-11-06 13:40:23 -0800

    flight api + info

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import FlightInfo from "./FlightInfo.js"
+import "./FlightInfo.css"
 import React, { useState } from "react"
 
 function App() {
-  const [hello, setHello] = useState()
+  const [flights, setFlights] = useState([])
 
-  function getPhoto() {
-    fetch("https://google.com", {dfgdf})
-    .then((response) => {
-      console.log(response)
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-Access-Token': 'bf2247f10f81ddb7135992e2bc77a70e',
+      'X-RapidAPI-Key': '5e901ebf68msh246b07db6a4063cp1e4ff4jsn1b22c15626ca',
+      'X-RapidAPI-Host': 'travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com'
+    }
+  };
+
+  const url = "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap?origin=YVR&page=1&currency=CAD&destination=-"
+
+  function getFlight() {
+    fetch(url, options)
+    .then((response) => response.json())
+    .then(json => {
+      const keys = Object.keys(json.data)
+      const n = 5
+      
+      const res = []
+      for (let i = 0; i < n; i++) {
+        const key = keys[i]
+        const flight = json.data[key][Object.keys(json.data[key])[0]]
+        res.push(flight)
+      }
+      setFlights(res)
     })
-
   }
 
   return (
-    <div className="App">
-      
+    <div className="container">
+      <button onClick={getFlight}>GetFlight</button>
+      {flights.map(flight => <FlightInfo {...flight} />)}
     </div>
   );
 }
